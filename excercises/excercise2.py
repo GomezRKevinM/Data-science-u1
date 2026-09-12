0dd369af7acdd925d34256884d52f7c8d610f078
kgomez <kgomezr2@unicartagena.edu.co>
2026-09-12 11:35:30 -0500

    feat: add 3th point with comments

--- excercises/excercise2.py
+++ excercises/excercise2.py
@@ -1,4 +1,5 @@
 # Declara variables de tipo entero, flotante, cadena y booleano. Imprime el tipo de cada una.
+from typing import cast
 
 edad = 24 #int
 peso = 62.5 #double
@@ -20,3 +21,8 @@ if nombre[-1] != " ": # verificando si el nombre termina con espacio
 else: # concatenar sin espacio porque el nombre ya lo trae
     full_name = nombre + apellido
 
+# Convierte una variable tipo string con valor numérico a entero y súmale 10.
+value_str = "10"
+cast_value = int(value_str) # convertimos la string a entero
+result = cast_value + 10 # Sumamos el valor convertido + 10
+print(f" resultado {cast_value} + 10 = {result}")
