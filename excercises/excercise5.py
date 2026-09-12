e33fb37070f5687b210ceea8baad697c9be40495
kgomez <kgomezr2@unicartagena.edu.co>
2026-09-12 14:28:18 -0500

    feat: use a for loop for print numbers of 1 to 10

--- excercises/excercise5.py
+++ excercises/excercise5.py
@@ -5,3 +5,7 @@ def isOddOrEver(number: int = 5):
     par = "es par" if number % 2 == 0 else "no es par"
     print(f"numero {number} {par}")
 
+# Usa un ciclo for para imprimir números del 1 al 10
+def printsNumberUpTo10():
+    for number in range(1,11): # recoremos del 1 al 11, solo imprime hasta el 10, cuando llega el 11 cierra el bucle
+        print(number)
