413a1f931a0dda75872e4a0cfbf560fdaaa64aa7
kgomez <kgomezr2@unicartagena.edu.co>
2026-09-12 12:42:31 -0500

    feat: use logic operators for check if a person is greater old and have license

--- excercises/excercise3.py
+++ excercises/excercise3.py
@@ -8,3 +8,11 @@ print(f"promedio de [{numero1},{numero2},{numero3}] = {promedio}")
 # Usa operadores de comparación para verificar si un número es mayor que 50
 numero=51
 print(f"{numero} es mayor que 50" if numero > 50 else f"{numero} es menor que 50")
+
+# Usa operadores lógicos para verificar si una persona es mayor de edad (≥18) y tiene licencia
+edad = 24
+es_mayor = "si" if edad >= 18 else "No"
+licencia = False
+tiene_licencia = "si" if licencia else "No"
+print(f"la persona es mayor de edad: {es_mayor}")
+print(f"la persona tiene Licencia: {tiene_licencia}")
