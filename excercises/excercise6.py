9d89938f1bc5cfe99fea5c118de53ae2b2cf609e
kgomez <kgomezr2@unicartagena.edu.co>
2026-09-12 15:24:20 -0500

    feat: simulate the data reading from a source and print each it in uppercase

--- excercises/excercise6.py
+++ excercises/excercise6.py
@@ -11,3 +11,11 @@ def calculateAverageAndShowApprovedStatus(notes: list[float|int] = [4.0, 3.5, 1.
     avg = fsum(notes) / len(notes) # obteniendo promedio
     print(f"El estudiante aprueba con: {avg}" if avg >= 3.0 else f"El estudiante reprueba con: {avg}" ) # mostrando resultado
 
+# Simula la lectura de datos desde una fuente (por ejemplo, una lista de nombres) e imprime cada uno en mayúsculas
+def iterateAListAPrintElementsToUpperCase(listName: list[str] = ['Kevin', 'Gary', 'Bella', 'Alexis', 'Sergio', 'Nathan']):
+    if not isinstance(listName, list): # validando parametro
+        raise TypeError("lista debe ser una lista")
+    if all(not isinstance(name, str) for name in listName): # validando elementos dentro de la lista
+        raise TypeError("los elementos de la lista deben ser cadenas de texto")
+
+    for name in listName: print(name.upper()) # recorrer e imprimir cada elementro de la lista en mayuscul
