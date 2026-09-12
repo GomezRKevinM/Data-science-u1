17de39a06d2616fd1a4116be3ce2c17ab70a633e
kgomez <kgomezr2@unicartagena.edu.co>
2026-09-12 14:49:01 -0500

    feat: create a conditional structure that classify a note (A, B, C, D)

--- excercises/excercise5.py
+++ excercises/excercise5.py
@@ -16,3 +16,22 @@ def printsRegresive():
     while contador > 0: # establecemos la condicion de ejecución
         print(contador) # imprimimos el contador
         contador -= 1 # le restamos 1 a contador y se repite el ciclo hasta que contador sea = 0
+
+# Crea una estructura condicional que clasifique una nota (A, B, C o D)
+def classifyNote(note: float | int = 5.0):
+    if not isinstance(note,float | int):
+        raise TypeError("El argumento espera un numero")
+
+    if note < 0 or note > 5:
+        raise ValueError("El argumento espera un numero entre 0 y 5")
+    match note:
+        case n if 4.0 <= n <= 5:
+            vowel = "A"
+        case n if 3.0 <= n < 4:
+            vowel = "B"
+        case n if 2.0 <= n < 3:
+            vowel = "C"
+        case _:
+            vowel = "D"
+
+    print(f"su nota se clafica en {vowel}")
