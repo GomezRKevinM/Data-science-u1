0d411efd5726ceb8e2b4aa187aa883029ec89906
kgomez <kgomezr2@unicartagena.edu.co>
2026-09-12 13:32:57 -0500

    feat: first point - show 3th item about fruit list

---
+++ excercises/excercise4.py
@@ -0,0 +1,4 @@
+# Crea una lista con 5 frutas e imprime la tercera
+fruit_list = ['Banana', 'Manzana', 'Kiwi', 'Sandia', 'Naranja']
+print(f"fruta #3 {fruit_list[-3]}") # podemos uasr 2 o -3 para representar el 3er elemento en esta ocación
+
