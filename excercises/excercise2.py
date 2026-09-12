e991e2ceeea159aaa51d8fddf92e1f9ad52d86aa
kgomez <kgomezr2@unicartagena.edu.co>
2026-09-12 11:25:34 -0500

    feat: the first point the excercise 2 has done

---
+++ excercises/excercise2.py
@@ -0,0 +1,13 @@
+# Declara variables de tipo entero, flotante, cadena y booleano. Imprime el tipo de cada una.
+
+edad = 24 #int
+peso = 62.5 #double
+nombre = "Kevin" #str
+mayor_edad = True #boolean
+
+# imprimiendo tipos
+
+print(f" edad --> {type(edad).__name__}")
+print(f" peso --> {type(peso).__name__}")
+print(f" nombre --> {type(nombre).__name__}")
+print(f" mayor_edad --> {type(mayor_edad).__name__}")
