e4304fbc52fdfa96676ff3d318b3cefdda6916ba
kgomez <kgomezr2@unicartagena.edu.co>
2026-09-12 12:36:27 -0500

    feat: compare a number and check if it is greater than 50

--- excercises/excercise3.py
+++ excercises/excercise3.py
@@ -4,3 +4,7 @@ numero2=4
 numero3=2
 promedio = (numero1+numero2+numero3)/3
 print(f"promedio de [{numero1},{numero2},{numero3}] = {promedio}")
+
+# Usa operadores de comparación para verificar si un número es mayor que 50
+numero=51
+print(f"{numero} es mayor que 50" if numero > 50 else f"{numero} es menor que 50")
