1dafeab4e914f5874fcd61cf0b4bdb5ed0a4bc62
kgomez <kgomezr2@unicartagena.edu.co>
2026-09-12 10:21:26 -0500

    feat: first point of the excercise 1

---
+++ excercise1.py
@@ -0,0 +1,6 @@
+## 1. Crea una lista que represente datos estructurados (por ejemplo: edades de estudiantes) y muestra su tipo de dato
+estudiantes_edades = [12, 15, 16, 9, 12, 13]
+print(f"Edades de etudiantes: {estudiantes_edades} -> data type= {type(estudiantes_edades).__name__}")
+
+## 2. Crea una variable tipo texto que represente un comentario libre de un usuario y cuenta cuantas palabras tiene
+## 3. Crea un diccionario con información estructurada de un estudiante (nombre, edad, carrera) e imprime cada valor por separado.
