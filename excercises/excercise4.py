f6e1780ef6b043aafef5adb2989b91b8e511f46a
kgomez <kgomezr2@unicartagena.edu.co>
2026-09-12 13:36:19 -0500

    feat: add a fruit to the fruit list

--- excercises/excercise4.py
+++ excercises/excercise4.py
@@ -2,3 +2,7 @@
 fruit_list = ['Banana', 'Manzana', 'Kiwi', 'Sandia', 'Naranja']
 print(f"fruta #3 {fruit_list[-3]}") # podemos uasr 2 o -3 para representar el 3er elemento en esta ocación
 
+# Añade un elemento a la lista anterior
+fruit_list.append("Fresa")
+print(fruit_list) #imprimimos para mostrar la fruta agregada
+
