d3a0c39fb74638703cf1c570689a6158c666bd92
kgomez <kgomezr2@unicartagena.edu.co>
2026-09-12 12:02:01 -0500

    feat: convert in functions the points for use in the main file

--- excercises/excercise2.py
+++ excercises/excercise2.py
@@ -1,28 +1,49 @@
 # Declara variables de tipo entero, flotante, cadena y booleano. Imprime el tipo de cada una.
-from typing import cast
+def showTypes(edad = None, peso = None, nombre = None):
+    if edad is None:
+        edad = 24
+    elif not isinstance(edad, int):
+        raise TypeError("edad debe ser un entero")
 
-edad = 24 #int
-peso = 62.5 #double
-nombre = "Kevin" #str
-mayor_edad = True #boolean
+    if peso is None:
+        peso = 67.8
+    elif not isinstance(peso, float):
+        raise TypeError("peso debe ser un decimal")
 
-# imprimiendo tipos
+    if nombre is None:
+        nombre = "Kevin"
+    elif not isinstance(nombre, str):
+        raise TypeError("nombre debe ser una cadena de texto")
 
-print(f" edad --> {type(edad).__name__}")
-print(f" peso --> {type(peso).__name__}")
-print(f" nombre --> {type(nombre).__name__}")
-print(f" mayor_edad --> {type(mayor_edad).__name__}")
+    mayor = edad >= 18 # comparamos la mayoria de edad
+
+    # imprimiendo tipos
+    print(" Variable     Tipo")
+    print(f" edad ------> {type(edad).__name__}")
+    print(f" peso ------> {type(peso).__name__}")
+    print(f" nombre ----> {type(nombre).__name__}")
+    print(f" mayor -----> {type(mayor).__name__}")
 
 # Escribe un programa que concatene nombre y apellido almacenados en variables
-apellido = "Gomez Rojas"
-nombre = "Kevin Manuel"
-if nombre[-1] != " ": # verificando si el nombre termina con espacio
-    full_name = nombre + " " + apellido
-else: # concatenar sin espacio porque el nombre ya lo trae
-    full_name = nombre + apellido
+def concatFullName(nombre= "Kevin Manuel", apellido = "Gomez Rojas"):
+    if not isinstance(nombre, str):
+        raise TypeError("nombre debe ser una cadena de texto")
+    if not isinstance(apellido, str):
+        raise TypeError("apellido debe ser una cadena de texto")
+
+    if nombre[-1] != " ": # verificando si el nombre termina con espacio
+        full_name = nombre + " " + apellido
+    else: # concatenar sin espacio porque el nombre ya lo trae
+        full_name = nombre + apellido
+    print(full_name)
 
 # Convierte una variable tipo string con valor numérico a entero y súmale 10.
-value_str = "10"
-cast_value = int(value_str) # convertimos la string a entero
-result = cast_value + 10 # Sumamos el valor convertido + 10
-print(f" resultado {cast_value} + 10 = {result}")
+def convertStrToInt(value = "10"):
+    if not isinstance(value, str): # validacion de tipo
+        raise TypeError("El valor debe ser una cadena de texto")
+    if not value.isnumeric(): # validacion de contenido
+        raise TypeError("El valor dentro de las cadenas de texto debe ser numerico")
+
+    cast_value = int(value) # convertimos la string a entero
+    result = cast_value + 10 # Sumamos el valor convertido + 10
+    print(f" resultado {cast_value} + 10 = {result}")
