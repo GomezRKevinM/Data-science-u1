d38aab03bd7f5b65f10dd75b4400a6fd3b6223d3
kgomez <kgomezr2@unicartagena.edu.co>
2026-09-12 12:02:39 -0500

    feat: tests the functions about excercise 2 in main

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-import excercise1 as e1
+import excercises.excercise1 as e1
+import excercises.excercise2 as e2
 
+print("="*30,"EJERCICIO 1","="*30)
 e1.createAListWichRepresentStructureData() # Lista de edades de estudiantes
 e1.createAVariableTypeText() # Comentario del usuario
 e1.createADictionaryWithStudentStructuredInformation() # Dicionario con información del estudiante
+
+# Ejercicio 2
+print("="*30,"EJERCICIO 2","="*30)
+e2.showTypes(10, 54.3, "El vato")
+e2.concatFullName("Victor Andry ","Duran Ortiz")
+e2.convertStrToInt("90")
