246238f81e0056c0b6e33a7336277b84b47a53ea
kgomez <kgomezr2@unicartagena.edu.co>
2026-09-12 12:26:55 -0500

    feat: first point about operadores excercise

---
+++ excercises/excercise3.py
@@ -0,0 +1,6 @@
+# Calcula el promedio de tres números usando operadores aritméticos
+numero1=5
+numero2=4
+numero3=2
+promedio = (numero1+numero2+numero3)/3
+print(f"promedio de [{numero1},{numero2},{numero3}] = {promedio}")
